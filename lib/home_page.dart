6c9d696980edfc5cf13eca756a538884842a02fd
unknown <engrsadiq13@gmail.com>
2021-08-07 01:11:30 -0700

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = 'Sadiq Amin';
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to $days days of flutter by $name'),
+        ),
+      ),
+    );
+  }
+}
