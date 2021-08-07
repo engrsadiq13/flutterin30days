f1a062405aaf1b31ba1bfab6a6b66e2c1eb84b8c
Sadiq Amin <engrsadiq13@gmail.com>
2021-08-07 01:27:30 -0700

    day 2 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,12 +7,15 @@ class HomePage extends StatelessWidget {
     String name = 'Sadiq Amin';
 
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
       body: Center(
         child: Container(
           child: Text('Welcome to $days days of flutter by $name'),
         ),
       ),
+      drawer: Drawer(),
     );
   }
 }
