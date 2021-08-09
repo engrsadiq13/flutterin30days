506b8ed7f8838dfd061ebc58ec661c946075b840
Sadiq Amin <engrsadiq13@gmail.com>
2021-08-09 01:34:09 -0700

    day 9 completed

---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog_1/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item.name} pressed ");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}",
+            style: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 18,
+                fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+}
